abb78ba0ce90a6cf473a7aff5442360240efee1c
kaidzn <83287458+kaidzn@users.noreply.github.com>
2021-06-17 10:12:16 +0600

    Create second_task.cpp

---
+++ second_task.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+   int flag = true;
+  while(flag){
+
+      int n, m ;
+     cout << "Input the number : ";
+    cin >> n;
+     m = n * (n+1)/2 ;
+    cout << "The result : "<< m << endl;
+    }
+  }
